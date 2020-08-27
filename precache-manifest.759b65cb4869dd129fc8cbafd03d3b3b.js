4b1791912e30080c83b3a7f7e3ed8090b4105db3
tlhysf <13beetyousuf@seecs.edu.pk>
2020-08-27 13:11:33 +0500

    Updates

---
+++ precache-manifest.759b65cb4869dd129fc8cbafd03d3b3b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c2a6e1efda83563bc7b804e4ac48c36",
+    "url": "/TodoList/index.html"
+  },
+  {
+    "revision": "1ce0ff33bd3da844c689",
+    "url": "/TodoList/static/css/main.1cbe413c.chunk.css"
+  },
+  {
+    "revision": "bedaea47f03a22b9cb93",
+    "url": "/TodoList/static/js/2.20536ee5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/TodoList/static/js/2.20536ee5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ce0ff33bd3da844c689",
+    "url": "/TodoList/static/js/main.350f1ea8.chunk.js"
+  },
+  {
+    "revision": "23d45432894410375589",
+    "url": "/TodoList/static/js/runtime-main.d0f80f2c.js"
+  }
+]);
